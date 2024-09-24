674a0236586a0884c127b6bf9257611e1d54baa5
Gabriel Acevedo <gacevedo@gmail.com>
2024-09-24 16:49:51 -0500

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 23

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -218,85 +218,51 @@ public class CustomerController {
    */
 @RequestMapping(value = "/saveSettings", method = RequestMethod.GET)
 public void saveSettings(HttpServletResponse httpResponse, WebRequest request) throws Exception {
-    if (!checkCookie(request)) {
+    // "Settings" will be stored in a cookie
+    // schema: base64(filename,value1,value2...), md5sum(base64(filename,value1,value2...))
+
+    if (!checkCookie(request)){
         httpResponse.getOutputStream().println("Error");
         throw new Exception("cookie is incorrect");
     }
 
     String settingsCookie = request.getHeader("Cookie");
     String[] cookie = settingsCookie.split(",");
-    if (cookie.length < 2) {
+    if(cookie.length<2) {
         httpResponse.getOutputStream().println("Malformed cookie");
         throw new Exception("cookie is incorrect");
     }
 
-    String base64txt = cookie[0].replace("settings=", "");
+    String base64txt = cookie[0].replace("settings=","");
 
+    // Check md5sum
     String cookieMD5sum = cookie[1];
     String calcMD5Sum = DigestUtils.md5Hex(base64txt);
-    if (!cookieMD5sum.equals(calcMD5Sum)) {
+    if(!cookieMD5sum.equals(calcMD5Sum))
+    {
         httpResponse.getOutputStream().println("Wrong md5");
         throw new Exception("Invalid MD5");
     }
 
+    // Now we can store on filesystem
     String[] settings = new String(Base64.getDecoder().decode(base64txt)).split(",");
+    // storage will have ClassPathResource as basepath
     ClassPathResource cpr = new ClassPathResource("./static/");
-    File file = new File(cpr.getPath() + settings[0]);
-    if (!file.exists()) {
+    File file = new File(cpr.getPath()+settings[0]);
+    if(!file.exists()) {
         file.getParentFile().mkdirs();
     }
 
-    FileOutputStream fos = new FileOutputStream(file, true);
-    String[] settingsArr = Arrays.copyOfRange(settings, 1, settings.length);
-    fos.write(String.join("\n", settingsArr).getBytes());
-    fos.write(("\n" + cookie[cookie.length - 1]).getBytes());
-    fos.close();
+    try (FileOutputStream fos = new FileOutputStream(file, true)) {
+        // First entry is the filename -> remove it
+        String[] settingsArr = Arrays.copyOfRange(settings, 1, settings.length);
+        // on setting at a line
+        fos.write(String.join("\n",settingsArr).getBytes());
+        fos.write(("\n"+cookie[cookie.length-1]).getBytes());
+    }
     httpResponse.getOutputStream().println("Settings Saved");
 }
 
-
-  /**
-   * Debug test for saving and reading a customer
-   *
-   * @param firstName String
-   * @param lastName String
-   * @param dateOfBirth String
-   * @param ssn String
-   * @param tin String
-   * @param phoneNumber String
-   * @param httpResponse
-   * @param request
-   * @return String
-   * @throws IOException
-   */
-  @RequestMapping(value = "/debug", method = RequestMethod.GET)
-  public String debug(@RequestParam String customerId,
-					  @RequestParam int clientId,
-					  @RequestParam String firstName,
-                      @RequestParam String lastName,
-                      @RequestParam String dateOfBirth,
-                      @RequestParam String ssn,
-					  @RequestParam String socialSecurityNum,
-                      @RequestParam String tin,
-                      @RequestParam String phoneNumber,
-                      HttpServletResponse httpResponse,
-                     WebRequest request) throws IOException{
-
-    // empty for now, because we debug
-    Set<Account> accounts1 = new HashSet<Account>();
-    //dateofbirth example -> "1982-01-10"
-    Customer customer1 = new Customer(customerId, clientId, firstName, lastName, DateTime.parse(dateOfBirth).toDate(),
-                                      ssn, socialSecurityNum, tin, phoneNumber, new Address("Debug str",
-                                      "", "Debug city", "CA", "12345"),
-                                      accounts1);
-
-    customerRepository.save(customer1);
-    httpResponse.setStatus(HttpStatus.CREATED.value());
-    httpResponse.setHeader("Location", String.format("%s/customers/%s",
-                           request.getContextPath(), customer1.getId()));
-
-    return customer1.toString().toLowerCase().replace("script","");
-  }
 
 	/**
 	 * Debug test for saving and reading a customer
@@ -380,4 +346,5 @@ public void saveSettings(HttpServletResponse httpResponse, WebRequest request) t
 	}
 
 }
+
 
