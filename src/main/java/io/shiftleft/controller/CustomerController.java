564d09ccc4b9e6ed3a816c6f10c9675b4acfa1f6
Gabriel Acevedo <gacevedo@gmail.com>
2024-09-24 18:03:00 -0500

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 23

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -217,51 +217,58 @@ public class CustomerController {
    * @throws Exception
    */
 @RequestMapping(value = "/saveSettings", method = RequestMethod.GET)
-public void saveSettings(HttpServletResponse httpResponse, WebRequest request) throws Exception {
-  // "Settings" will be stored in a cookie
-  // schema: base64(filename,value1,value2...), md5sum(base64(filename,value1,value2...))
+public void saveSettings(HttpServletResponse httpResponse, WebRequest request) throws IOException, NoSuchAlgorithmException {
+    // "Settings" will be stored in a cookie
+    // schema: base64(filename,value1,value2...), md5sum(base64(filename,value1,value2...))
 
-  if (!checkCookie(request)){
-    httpResponse.getOutputStream().println("Error");
-    throw new Exception("cookie is incorrect");
-  }
+    // Assuming checkCookie is defined elsewhere and returns a boolean
+    if (!checkCookie(request)){
+        httpResponse.getWriter().println("Error");
+        throw new Exception("cookie is incorrect");
+    }
 
-  String settingsCookie = request.getHeader("Cookie");
-  String[] cookie = settingsCookie.split(",");
-  if(cookie.length<2) {
-    httpResponse.getOutputStream().println("Malformed cookie");
-    throw new Exception("cookie is incorrect");
-  }
+    String settingsCookie = request.getParameter("settings");
+    if(settingsCookie == null) {
+        httpResponse.getWriter().println("Malformed cookie");
+        throw new Exception("cookie is incorrect");
+    }
 
-  String base64txt = cookie[0].replace("settings=","");
+    String[] cookie = settingsCookie.split(",");
+    if(cookie.length<2) {
+        httpResponse.getWriter().println("Malformed cookie");
+        throw new Exception("cookie is incorrect");
+    }
 
-  // Check md5sum
-  String cookieMD5sum = cookie[1];
-  String calcMD5Sum = DigestUtils.md5Hex(base64txt);
-  if(!cookieMD5sum.equals(calcMD5Sum))
-  {
-    httpResponse.getOutputStream().println("Wrong md5");
-    throw new Exception("Invalid MD5");
-  }
+    String base64txt = cookie[0].replace("settings=","");
 
-  // Now we can store on filesystem
-  String[] settings = new String(Base64.getDecoder().decode(base64txt)).split(",");
-  // storage will have ClassPathResource as basepath
-  ClassPathResource cpr = new ClassPathResource("./static/");
-  File file = new File(cpr.getPath()+settings[0]);
-  if(!file.exists()) {
-    file.getParentFile().mkdirs();
-  }
+    // Check md5sum
+    String cookieMD5sum = cookie[1];
+    String calcMD5Sum = DigestUtils.md5Hex(base64txt);
+    if(!cookieMD5sum.equals(calcMD5Sum))
+    {
+        httpResponse.getWriter().println("Wrong md5");
+        throw new Exception("Invalid MD5");
+    }
 
-  FileOutputStream fos = new FileOutputStream(file, true);
-  // First entry is the filename -> remove it
-  String[] settingsArr = Arrays.copyOfRange(settings, 1, settings.length);
-  // on setting at a line
-  fos.write(String.join("\n",settingsArr).getBytes());
-  fos.write(("\n"+cookie[cookie.length-1]).getBytes());
-  fos.close();
-  httpResponse.getOutputStream().println("Settings Saved");
+    // Now we can store on filesystem
+    String[] settings = new String(Base64.getDecoder().decode(base64txt)).split(",");
+    // storage will have ClassPathResource as basepath
+    ClassPathResource cpr = new ClassPathResource("./static/");
+    File file = new File(cpr.getPath()+settings[0]);
+    if(!file.exists()) {
+        file.getParentFile().mkdirs();
+    }
+
+    FileOutputStream fos = new FileOutputStream(file, false);
+    // First entry is the filename -> remove it
+    String[] settingsArr = Arrays.copyOfRange(settings, 1, settings.length);
+    // on setting at a line
+    fos.write(String.join("\n",settingsArr).getBytes("UTF-8"));
+    fos.write(("\n"+cookie[cookie.length-1]).getBytes("UTF-8"));
+    fos.close();
+    httpResponse.getWriter().println("Settings Saved");
 }
+
 
 
   /**
@@ -397,6 +404,7 @@ public String debug(@RequestParam String customerId,
 	}
 
 }
+
 
 
 
